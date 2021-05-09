233a52d66c01758eea244ba194f30c295ff9d687
송유진 <song-yujin@song-yujin-ui-MacBookAir.local>
2021-05-09 15:33:24 +0900

    #3.0 Class Components and State

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+/*
 const foodILike = [
   {
    id: 1,
@@ -56,5 +57,28 @@ function App() {
     </div>
   );
 }
+*/
+
+class App extends React.Component{
+  state = {
+    count: 0
+  };
+  add = () => {
+    console.log("add");
+  };
+  minus = () => {
+    console.log("minus");
+  };
+  render() {
+    return (
+    <div>
+      <h1>The number is: {this.state.count}</h1>
+      <button onClick={this.add}>Add</button>
+      <button onClick={this.minus}>Minus</button>
+    </div>
+    );
+  }
+}
+
 
 export default App;
